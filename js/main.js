51714d8c225d91960664d749a57b5cb882e4f5fe
chertoliy <104504367+chertoliy@users.noreply.github.com>
2023-03-21 00:19:07 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
-$(function () {
+// Global vriables
+const openMobileMenuBtn = document.querySelector('.menu__btn-mobile');
+const mobileMenu = document.querySelector('.mobile-menu');
+const body = document.querySelector('body');
+const closeMobileMenuBtn = document.querySelector('.mobile-menu__btn');
+const mobileMenuLinks = document.getElementsByClassName('mobile-menu__sections-link');
 
-   $('.menu__btn-mobile').on('click', function () {
-       $('.mobile-menu').removeClass('mobile-menu--close');
-   });
-   $('.mobile-menu__btn, .mobile-menu__sections-link, section').on('click', function () {
-       $('.mobile-menu').addClass('mobile-menu--close');
-   });
+// Open mobile menu
+function openMobileMenu(event) {
+   mobileMenu.classList.remove('mobile-menu--close');
+   event.stopPropagation();
+};
 
-})
+openMobileMenuBtn.onclick = openMobileMenu;
+
+
+// Close mobile menu
+function closeMobileMenu() {
+   mobileMenu.classList.add('mobile-menu--close');
+};
+
+body.onclick = closeMobileMenu;
+closeMobileMenuBtn.onclick = closeMobileMenu;
+
+for (mobileMenuLink of mobileMenuLinks) {
+   mobileMenuLink.onclick = closeMobileMenu;
+};
